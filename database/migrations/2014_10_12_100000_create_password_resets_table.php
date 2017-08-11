e014b00dd553e99f9a3525367f8e94221e50c7ba
管宜尧 <guanyiyao@yunsom.com>
2017-08-11 13:44:51 +0800

    bugfix for database migrate script: email too long as unique index

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,7 +14,7 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('wz_password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
+            $table->string('email', 200)->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
--- database/migrations/2017_08_10_093939_create_categories_table.php
+++ database/migrations/2017_08_10_093939_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('wz_users', function (Blueprint $table) {
+        Schema::create('wz_categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email', 200)->unique();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('wz_users');
+        Schema::dropIfExists('wz_categories');
     }
 }
