5567d8545779f06099c75141c571edc4c0dc3b38
管宜尧 <guanyiyao@yunsom.com>
2017-09-07 00:07:21 +0800

    文档浏览页流式布局

--- resources/views/layouts/project.blade.php
+++ resources/views/layouts/project.blade.php
@@ -21,7 +21,7 @@
 
                 </ul>
             </div>
-            <div class="col-lg-8 col-md-9 col-sm-9">
+            <div class="col-lg-9 col-md-9 col-sm-9">
                 <nav class="wz-page-control clearfix">
                     <div class="btn-group wz-nav-control">
                         @can('page-add', $project)
