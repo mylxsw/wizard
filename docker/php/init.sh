42944f290b3559a2baa857ead9e9624b034ab249
管宜尧 <guanyiyao@yunsom.com>
2018-04-27 09:41:04 +0800

    docker命令修正

--- docker/php/init.sh
+++ docker/php/init.sh
@@ -3,5 +3,5 @@
 cd /webroot/wizard && php /usr/bin/composer install
 
 cp .env.docker .env
-php /webroot/wizard/artisan migrate
+php /webroot/wizard/artisan migrate --force
 php /webroot/wizard/artisan storage:link
