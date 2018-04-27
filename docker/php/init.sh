25e145d093540dae2a968375f317118cd8cf9506
管宜尧 <guanyiyao@yunsom.com>
2018-04-27 10:08:11 +0800

    bugfix docker can not run because file system permission problem

--- docker/php/init.sh
+++ docker/php/init.sh
@@ -3,5 +3,7 @@
 cd /webroot/wizard && php /usr/bin/composer install
 
 cp /webroot/wizard/.env.docker /webroot/wizard/.env
+chown www-data:www-data -R /webroot/wizard/
+
 php /webroot/wizard/artisan migrate --force
 php /webroot/wizard/artisan storage:link
