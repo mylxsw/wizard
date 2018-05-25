d9f98bc1646b2128c1c02cbaa95ee1bdf46eb0db
ro4 <thefrp@gmail.com>
2018-05-25 18:14:01 +0800

    add tags ajax api

--- resources/views/components/tags.blade.php
+++ resources/views/components/tags.blade.php
@@ -5,43 +5,47 @@
 @push('script')
 @if(!Auth::guest())
     <script>
-        jQuery(".tm-input").tagsManager({
-            prefilled: "{{$pageItem->tags->pluck('name')->implode(',')}}",
-            CapitalizeFirstLetter: false,
-            AjaxPush: '/tag',
-            AjaxPushAllTags: true,
-            AjaxPushParameters: {'p': {{$pageID}} },
-            delimiters: [9, 13, 44],
-            backspace: [8],
-            blinkBGColor_1: '#FFFF9C',
-            blinkBGColor_2: '#CDE69C',
-            hiddenTagListName: 'hiddenTagListA',
-            hiddenTagListId: null,
-            deleteTagsOnBackspace: true,
-            tagsContainer: null,
-            tagCloseIcon: '×',
-            tagClass: '',
-            validator: null,
-            onlyTagList: false
+        $(function () {
+            $(".tm-input").tagsManager({
+                prefilled: "{{$pageItem->tags->pluck('name')->implode(',')}}",
+                CapitalizeFirstLetter: false,
+                AjaxPush: '/tag',
+                AjaxPushAllTags: true,
+                AjaxPushParameters: {'p': {{$pageID}} },
+                delimiters: [9, 13, 44],
+                backspace: [8],
+                blinkBGColor_1: '#FFFF9C',
+                blinkBGColor_2: '#CDE69C',
+                hiddenTagListName: 'hiddenTagListA',
+                hiddenTagListId: null,
+                deleteTagsOnBackspace: true,
+                tagsContainer: null,
+                tagCloseIcon: '×',
+                tagClass: 'tm-tag-success',
+                validator: null,
+                onlyTagList: false
+            });
         });
     </script>
 @else
     <script>
-        jQuery(".tm-input").tagsManager({
-            prefilled: "{{$pageItem->tags->pluck('name')->implode(',')}}",
-            CapitalizeFirstLetter: false,
-            delimiters: [9, 13, 44],
-            backspace: [8],
-            blinkBGColor_1: '#FFFF9C',
-            blinkBGColor_2: '#CDE69C',
-            hiddenTagListName: 'hiddenTagListA',
-            hiddenTagListId: null,
-            deleteTagsOnBackspace: true,
-            tagsContainer: null,
-            tagCloseIcon: '×',
-            tagClass: 'tm-tag-disabled',
-            validator: null,
-            onlyTagList: false
+        $(function () {
+            $(".tm-input").tagsManager({
+                prefilled: "{{$pageItem->tags->pluck('name')->implode(',')}}",
+                CapitalizeFirstLetter: false,
+                delimiters: [9, 13, 44],
+                backspace: [8],
+                blinkBGColor_1: '#FFFF9C',
+                blinkBGColor_2: '#CDE69C',
+                hiddenTagListName: 'hiddenTagListA',
+                hiddenTagListId: null,
+                deleteTagsOnBackspace: true,
+                tagsContainer: null,
+                tagCloseIcon: '×',
+                tagClass: 'tm-tag-disabled',
+                validator: null,
+                onlyTagList: false
+            });
         });
     </script>
 @endif
