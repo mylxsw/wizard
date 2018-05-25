1a64e4f76a14bf2275d16c5d7eb3ab6be1758cb9
ro4 <thefrp@gmail.com>
2018-05-25 17:57:52 +0800

    add tags ajax api

--- resources/views/components/tags.blade.php
+++ resources/views/components/tags.blade.php
@@ -1,27 +1,48 @@
-<p class="wz-document-header">
-        <input type="text" name="tags" placeholder="Tags"  class="tm-input"/>
-</p>
+<div class="card-body">
+    <input type="text" name="tags" placeholder="Tags" class="tm-input" @if(Auth::guest()) style="display: none;" @endif/>
+</div>
 
 @push('script')
-<script>
-    jQuery(".tm-input").tagsManager({
-        prefilled: "{{$pageItem->tags->pluck('name')->implode(',')}}",
-        CapitalizeFirstLetter: false,
-        AjaxPush: '/tag',
-        AjaxPushAllTags: true,
-        AjaxPushParameters: { 'p': {{$pageID}} },
-        delimiters: [9, 13, 44],
-        backspace: [8],
-        blinkBGColor_1: '#FFFF9C',
-        blinkBGColor_2: '#CDE69C',
-        hiddenTagListName: 'hiddenTagListA',
-        hiddenTagListId: null,
-        deleteTagsOnBackspace: true,
-        tagsContainer: null,
-        tagCloseIcon: '×',
-        tagClass: '',
-        validator: null,
-        onlyTagList: false
-    });
-</script>
+@if(!Auth::guest())
+    <script>
+        jQuery(".tm-input").tagsManager({
+            prefilled: "{{$pageItem->tags->pluck('name')->implode(',')}}",
+            CapitalizeFirstLetter: false,
+            AjaxPush: '/tag',
+            AjaxPushAllTags: true,
+            AjaxPushParameters: {'p': {{$pageID}} },
+            delimiters: [9, 13, 44],
+            backspace: [8],
+            blinkBGColor_1: '#FFFF9C',
+            blinkBGColor_2: '#CDE69C',
+            hiddenTagListName: 'hiddenTagListA',
+            hiddenTagListId: null,
+            deleteTagsOnBackspace: true,
+            tagsContainer: null,
+            tagCloseIcon: '×',
+            tagClass: '',
+            validator: null,
+            onlyTagList: false
+        });
+    </script>
+@else
+    <script>
+        jQuery(".tm-input").tagsManager({
+            prefilled: "{{$pageItem->tags->pluck('name')->implode(',')}}",
+            CapitalizeFirstLetter: false,
+            delimiters: [9, 13, 44],
+            backspace: [8],
+            blinkBGColor_1: '#FFFF9C',
+            blinkBGColor_2: '#CDE69C',
+            hiddenTagListName: 'hiddenTagListA',
+            hiddenTagListId: null,
+            deleteTagsOnBackspace: true,
+            tagsContainer: null,
+            tagCloseIcon: '×',
+            tagClass: 'tm-tag-disabled',
+            validator: null,
+            onlyTagList: false
+        });
+    </script>
+@endif
 @endpush
