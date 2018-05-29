da098c9da86a6d34faa1fede5ca355c4c7bbda68
ro4 <thefrp@gmail.com>
2018-05-29 18:26:18 +0800

    done

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -12,10 +12,26 @@ class TagController extends Controller
 
     public function store(Request $request)
     {
+        $page_id = $request->input('p');
+        $tags    = $request->input('tags');
+        $this->validateParameters(
+            [
+                'page_id' => $page_id,
+                'tags'    => $tags,
+            ],
+            [
+                'page_id' => "required|integer",
+                'tags'    => 'nullable|max:500',
+            ],
+            [
+                'tags.required' => '标签不能为空',
+                'tags.between'  => '标签最大不能超过500字符',
+            ]
+        );
         /** @var Document $page */
-        $page = Document::findOrFail($request->input('p'));
-        $names = explode(',', $request->input('tags'));
-        $tags = [];
+        $page  = Document::findOrFail($page_id);
+        $names = explode(',', $tags);
+        $tags  = [];
         foreach ($names as $name) {
             $tags[] = Tag::firstOrCreate(['name' => $name])->toArray();
         }
