92bd092a91ba5937b503593e38a256fe441f0076
管宜尧 <guanyiyao@yunsom.com>
2018-06-03 16:02:27 +0800

    swagger文档导航图标修改

--- public/assets/js/navigator-tree.js
+++ public/assets/js/navigator-tree.js
@@ -59,7 +59,7 @@ $.wz.navigator_tree = function (left_nav) {
     childrenShow(left_nav.children('li'));
 
     left_nav.find('li:not(.wz-has-child)').map(function () {
-        $(this).prepend('<a class="fa ' + ($(this).data('type') === 'swagger' ? 'fa-file-code-o' : 'fa-file-text-o') + ' wz-nav-fold" href="javascript:;"></a>');
+        $(this).prepend('<a class="fa ' + ($(this).data('type') === 'swagger' ? 'fa-code' : 'fa-file-text-o') + ' wz-nav-fold" href="javascript:;"></a>');
     });
 
 };
