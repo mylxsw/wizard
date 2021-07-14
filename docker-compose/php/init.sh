44dfd5ff9f07c69d3ecfb48a9d2973a8c54870e4
普罗米修斯 <mylxsw@puluomixiusideMacBook-Pro.local>
2021-07-14 16:33:41 +0800

    add -vvvv to docker-compose php

--- docker-compose/php/init.sh
+++ docker-compose/php/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd /webroot/wizard && php /usr/bin/composer install --prefer-dist --ignore-platform-reqs --no-scripts
+cd /webroot/wizard && php /usr/bin/composer install --prefer-dist --ignore-platform-reqs --no-scripts -vvv
 
 cp /webroot/wizard/docker-compose/.env.docker /webroot/wizard/.env
 chown www-data:www-data -R /webroot/wizard/
